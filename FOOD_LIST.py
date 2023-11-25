abaff3288a326624bb94dee240bb3a2b573dd2ae
draco1411 <jbrolin815@gmail.com>
2023-11-24 19:09:25 -0600

    Some more additions

---
+++ FOOD_LIST.py
@@ -0,0 +1,8 @@
+FOOD_LIST = {
+    "1": [190, 4, 23, 16],  # Greek Yogurt w/ Honey
+    "2": [450, 20, 28, 44],  # P-Shake
+    "3": [450, 11, 75, 14],  # Standard Cereal
+    "4": [310, 13, 44, 10],  # Bagel w/ Butter
+    "5": [370, 25, 4, 27],  # 4 Egg w/ 14g cheese
+    "6": [150, 8, 11, 8]  # 8 oz Milk
+}
--- diet.py
+++ diet.py
@@ -1,50 +1,67 @@
-# import json as js
 import datetime as dt
+import csv
+import os
+import shutil
+from FOOD_LIST import FOOD_LIST
+
+# Time retrieval and formatting
+d = dt.date.today()
+date = d.strftime("%m-%d-%Y")
+
+current_time = dt.datetime.now().time()
+time = current_time.strftime("%I:%M%p")
 
 
 def quick_add():
-    print("This returns for quick add")
+    indicator = input("Quick add:  ")
+    N_vector = FOOD_LIST[indicator]
+    row = (date, time, N_vector)
+    with open('food_log.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(row)
     return None
 
 
 def manual_entry():
-    print("This returns for manual_entry")
+    cals = int(input("Cal:  "))
+    fat = int(input("Fat:  "))
+    carbs = int(input("Carbs:  "))
+    protein = int(input("Protein:  "))
+    N_vector = [cals, fat, carbs, protein]
+    row = (date, time, N_vector)
+    with open('food_log.csv', 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow(row)
     return None
 
 
 def weigh_in():
     daily_weight = int(input("Weight:  "))
-    date = dt.date.today()
-    with open('diet.csv', 'a') as file:
-        file.write(f"{date},{daily_weight}")
-    print(daily_weight)
-    print(date)
+    row = (date, daily_weight)
+    with open('weight.csv', 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow(row)
     return None
 
 
-menu = {
-    "1": "Quick add",
-    "2": "Manual",
-    "3": "Weigh In"
-}
+def stats():
+    os.system('clear')
+    with open('food_log.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            print(row)
+    txt = "This is the weather"
+    print(txt.center(shutil.get_terminal_size().columns))
 
-menu_selector = [quick_add, manual_entry, weigh_in]
 
-QUICK_FOOD_LIST = {
-    "1": [],  # Greek Yogurt w/ Honey
-    "2": [],  # P-Shake
-    "3": [],  # Two Burger Dinner
-    "4": [],  # 170g Cottage Cheese
-    "5": [],  # Standard Cereal
-    "6": [],  # 4 Egg w/ Cheese Lunch
-
-}
+menu_selector = [quick_add, manual_entry, weigh_in, stats]
 
 
 menu_selection = int(input("Select an option: \n"
                            "1: Quick add. \n"
                            "2: Manual Entry \n"
                            "3: Weigh In \n"
+                           "4: Stats \n"
                            "Select:  "))
 
 
