fc23d5921002d242e3b2940c54321cd127b6ddcc
draco1411 <jbrolin815@gmail.com>
2023-11-30 23:03:57 -0600

    raps and things

--- diet.py
+++ diet.py
@@ -1,24 +1,21 @@
 import datetime as dt
-import csv
+import json
 import os
-import shutil
+# import shutil
 from FOOD_LIST import FOOD_LIST
 
 # Time retrieval and formatting
 d = dt.date.today()
 date = d.strftime("%m-%d-%Y")
 
-current_time = dt.datetime.now().time()
-time = current_time.strftime("%I:%M%p")
-
 
 def quick_add():
     indicator = input("Quick add:  ")
     N_vector = FOOD_LIST[indicator]
-    row = (date, time, N_vector)
-    with open('food_log.csv', 'a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(row)
+    row = {"date": date, "N_vector": N_vector}
+    with open('log.json', 'a') as file:
+        json.dump(row, file)
+        file.write('\n')
     return None
 
 
@@ -28,42 +25,44 @@ def manual_entry():
     carbs = int(input("Carbs:  "))
     protein = int(input("Protein:  "))
     N_vector = [cals, fat, carbs, protein]
-    row = (date, time, N_vector)
-    with open('food_log.csv', 'a') as file:
-        writer = csv.writer(file)
-        writer.writerow(row)
+    row = {"date": date, "N_vector": N_vector}
+    with open('log.json', 'a') as file:
+        json.dump(row, file)
+        file.write('\n')
     return None
 
 
 def weigh_in():
     daily_weight = int(input("Weight:  "))
-    row = (date, daily_weight)
-    with open('weight.csv', 'a') as file:
-        writer = csv.writer(file)
-        writer.writerow(row)
+    row = {"date": date, "weight": daily_weight}
+    with open('weight.json', 'a') as file:
+        json.dump(row, file)
+        file.write('\n')
     return None
 
 
 def stats():
     os.system('clear')
-    with open('food_log.csv', 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            print(row)
-    txt = "This is the weather"
-    print(txt.center(shutil.get_terminal_size().columns))
+    with open('weight.json', 'r') as file:
+        data = [json.loads(line) for line in file]
+        daily_weight = [data[i]['weight'] for i in range(len(data))]
+        print(daily_weight)
+    with open('log.json', 'r') as file:
+        data = [json.loads(line) for line in file]
+        partial = [data[i]['N_vector'] for i in range(len(data))]
+        total_weight = [partial[i][0] for i in range(len(partial))]
+        print(sum(total_weight))
 
 
 menu_selector = [quick_add, manual_entry, weigh_in, stats]
 
-
+os.system('clear')
 menu_selection = int(input("Select an option: \n"
                            "1: Quick add. \n"
                            "2: Manual Entry \n"
                            "3: Weigh In \n"
                            "4: Stats \n"
                            "Select:  "))
-
 
 function = menu_selector[menu_selection - 1]
 function()
