7f9ff09e4eec919a57f4d3f72562556fc1ffc362
draco1411 <jbrolin815@gmail.com>
2023-12-02 22:34:34 -0600

    Major problems getting solved

--- diet.py
+++ diet.py
@@ -13,7 +13,7 @@ def quick_add():
     indicator = input("Quick add:  ")
     N_vector = FOOD_LIST[indicator]
     row = {"date": date, "N_vector": N_vector}
-    with open('log.json', 'a') as file:
+    with open('daily_log.json', 'a') as file:
         json.dump(row, file)
         file.write('\n')
     return None
@@ -26,9 +26,20 @@ def manual_entry():
     protein = int(input("Protein:  "))
     N_vector = [cals, fat, carbs, protein]
     row = {"date": date, "N_vector": N_vector}
-    with open('log.json', 'a') as file:
+    with open('daily_log.json', 'a') as file:
         json.dump(row, file)
         file.write('\n')
+    return None
+
+
+def aux_file_writer():
+    with open('/Users/jackbrolin/diet/master_log.json', 'a') as file:
+        for i in range(1,  31):
+            current_date = dt.date(2024, 4, i)
+            row = {"date": str(current_date.strftime("%m-%d-%Y")), "N-vector":
+                   [0, 0, 0, 0]}
+            json.dump(row, file)
+            file.write('\n')
     return None
 
 
@@ -41,28 +52,41 @@ def weigh_in():
     return None
 
 
-def stats():
-    os.system('clear')
-    with open('weight.json', 'r') as file:
+def get_weight():
+    with open('/Users/jackbrolin/diet/weight.json', 'r') as file:
         data = [json.loads(line) for line in file]
         daily_weight = [data[i]['weight'] for i in range(len(data))]
         print(daily_weight)
-    with open('log.json', 'r') as file:
+    return None
+
+
+def get_daily_food():
+    with open('/Users/jackbrolin/diet/daily_log.json', 'r') as file:
         data = [json.loads(line) for line in file]
         partial = [data[i]['N_vector'] for i in range(len(data))]
-        total_weight = [partial[i][0] for i in range(len(partial))]
-        print(sum(total_weight))
+        total_cals = [partial[i][0] for i in range(len(partial))]
+        print(sum(total_cals))
+    return None
 
 
-menu_selector = [quick_add, manual_entry, weigh_in, stats]
+def stats():
+    os.system('clear')
+    get_weight()
+    get_daily_food()
+    return None
+
+
+# menu_selector = [quick_add, manual_entry, weigh_in, stats]
 
 os.system('clear')
-menu_selection = int(input("Select an option: \n"
-                           "1: Quick add. \n"
-                           "2: Manual Entry \n"
-                           "3: Weigh In \n"
-                           "4: Stats \n"
-                           "Select:  "))
+# menu_selection = int(input("Select an option: \n"
+#                            "1: Quick add. \n"
+#                            "2: Manual Entry \n"
+#                            "3: Weigh In \n"
+#                            "4: Stats \n"
+#                            "Select:  "))
 
-function = menu_selector[menu_selection - 1]
-function()
+aux_file_writer()
+
+# function = menu_selector[menu_selection - 1]
+# function()
